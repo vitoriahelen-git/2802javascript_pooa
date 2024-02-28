2dde8b1acff52c7b70b8cb0c6fbf2135315a3e4f
vitoriahellen3004@gmail.com <vitoriahellen3004@gmail.com>
2024-02-28 15:30:24 -0300

    js(funcoes): fim de 1.6.1

---
+++ index.js
@@ -0,0 +1,92 @@
+//comentario 
+
+//funcao 
+function hello(){
+    console.log('oi')
+}
+hello('oi')
+function hello(nome){
+    console.log('oi, ' + nome)
+}
+hello('Rodrigo')
+    
+
+
+
+// v1 = []
+// v1[0] = 3.4
+// console.log(v1.length)
+// v1[10] = 2 
+// console.log(v1.length)
+// v1[2] = 'abc'
+// console.log(v1.length)
+// for(let i = 0; i < v1.length; i++){
+//     console.log(v1[i])
+// }
+
+
+// console.log([] == [])
+// console.log([] == false)
+// console.log(null == undefined)
+// console.log(null == null)
+// console.log(21 == [2, 1])
+// console.log(1 == [21])
+// console.log(1 == [2, 1])
+// console.log(1 == [1, 2])
+// console.log(1 == [2])
+// console.log(1 == [1])
+// console.log(1 == true)
+// console.log(1 === '1')
+// console.log(1 == 1)
+// console.log(1 == '1')
+// console.log(1 === 1)
+
+
+// const n1 = 2 
+// const n2 = '3'
+// const n3 = n1 + n2
+// console.log(n3)
+// // coerção explicita
+// const n4 = n1 + Number(n2)
+// console.log(n4)
+
+// var idade = 18  
+// console.log(`Oi, ${nome}`)
+// if(idade>=18){
+//     var nome = 'João'  
+//     console.log(`Parabéns, ${nome}. Voce pode dirigir`) 
+// }
+// console.log(`Até mais, ${nome}`)
+
+
+// var linguagem = "JavaScript"
+// console.log(`Aprendendo ${linguagem}`)
+// var linguagem = "Java"
+// console.log(`Aprendendo ${linguagem}`)
+
+
+// let nome = "Vitoria"
+// console.log(nome)
+// nome = "Pedro"
+// console.log(nome)
+
+// let a = 2 
+// console.log(typeof(a))
+// a = "abc"
+// console.log(typeof(a))
+// a = true 
+// console.log(typeof(a))
+
+
+// const nome = "José"
+// console.log(nome)
+// //nome = "Pedro"
+// const endereco =  'Rua B, 21'
+// console.log(endereco)
+// const sobrenome = `Veloso`
+// console.log(sobrenome)
+// //Me chamo Vitoria. Meu sobrenome 
+// console.log('Me chamo' + nome + '. Meu sobrenome é' + sobrenome)
+// console.log(`Me chamo ${nome}. Meu sobrenome é ${sobrenome}.`)
+// const idade = 22 
+// const passei
